4f8edc2b09c3fa0d3616644a6852f7a1ab727d10
Meisuleen Kased <meisuleen@gmail.com>
2019-08-26 18:50:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,36 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-  get '/' do
+  get '/recipe/new' do #loads new form
+    erb :new
+  end
+
+  get '/recipe' do  #loads index pg
     @recipes = Recipe.all
     erb :index
   end
+  
+  get '/recipe/:id' do  #loads display pg
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :display
+  end
 
-  # get '/index/new' do
-  #     erb :new
-  # end
+  get '/recipe/:id/edit' do  #edit recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
 
-  get '/index' do
-    @recipe = Recipe.create(name: params[:name], recipe: params[:recipe])
+  patch '/recipe/:id' do  #update recipe
+    @recipe = Recipe.find_by_id(params[:is])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to '/recipe/#{@recipe.id}'
+  end
+
+  get '/recipe' do  #creates a recipe
+    @recipe = Recipe.create(:name => params[:name], :recipe => params[:recipe])
     @recipe.save
   end
 
